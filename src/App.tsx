5d69c489c1e46192979f79d15f63573d0fdee557
CleverDarkSolutions <RotmgADAS@o2.pl>
2024-06-14 01:28:21 +0200

    Clean up template and check on some git issues

--- src/App.tsx
+++ src/App.tsx
@@ -5,16 +5,7 @@ import './index.css';
 function App () {
   return (
     <div className="App">
-      <div className="text-center">
-        <header className="bg-red-500 text-white p-6">
-          <h1 className="text-3xl font-bold">Tailwind works</h1>
-        </header>
-        <main className="p-4">
-          <p className="text-lg">
-                      Start editing to see some magic happen!
-          </p>
-        </main>
-      </div>
+
     </div>
   );
 }
