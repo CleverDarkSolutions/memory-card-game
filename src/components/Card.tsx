20a1052d8b04f6cb28b392d995ead74eb402153b
CleverDarkSolutions <RotmgADAS@o2.pl>
2024-06-15 01:26:15 +0200

    Flipping effect works and added some images

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -10,13 +10,13 @@ export type CardProps = {
 }
 const Card = (props: CardProps) => {
   return (
-    <div className={`w-full h-40 m-4 border-2 border-blue-500 transform transition-transform duration-1000 ${props.flipped ? 'rotate-y-180' : ''}`}>
+    <div className={`w-24 h-36 m-4 transform transition-transform duration-1000 ${props.flipped ? 'rotate-y-180' : ''}`}>
       {props.flipped ? (
-        <div className="h-full backface-hidden flex items-center justify-center bg-cover bg-[url('https://upload.wikimedia.org/wikipedia/commons/thumb/d/d4/Card_back_01.svg/703px-Card_back_01.svg.png')]">
+        <div className="h-full backface-hidden flex items-center justify-center border-black border-2 p-4 rounded">
+          <img src={props.image}/>
         </div>
       ) : (
-        <div className="h-full backface-hidden flex items-center justify-center" onClick={() => props.flip(props.id)}>
-          {props.name}
+        <div className="h-full backface-hidden flex items-center justify-center bg-contain bg-no-repeat bg-[url('https://upload.wikimedia.org/wikipedia/commons/thumb/d/d4/Card_back_01.svg/703px-Card_back_01.svg.png')]" onClick={() => props.flip(props.id)}>
         </div>
       )}
     </div>
--- src/components/CardGrid.tsx
+++ src/components/CardGrid.tsx
@@ -12,59 +12,115 @@ const CardGrid = () => {
   const [cards, setCards] = useState<Card[]>([
     {
       name: 'Card 1',
-      image: 'string',
+      image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTKfxxgtvjoywpYYFjqCM2IByvFIxA6n40Wtw&s.jpg',
       flipped: false,
       id: 1,
       pairId: 1,
     },
     {
       name: 'Card 2',
-      image: 'string',
+      image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTKfxxgtvjoywpYYFjqCM2IByvFIxA6n40Wtw&s.jpg',
       flipped: false,
       id: 2,
       pairId: 1,
     },
     {
       name: 'Card 3',
-      image: 'string',
+      image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/95/Vue.js_Logo_2.svg/1200px-Vue.js_Logo_2.svg.png',
       flipped: false,
       id: 3,
       pairId: 2,
     },
     {
       name: 'Card 4',
-      image: 'string',
+      image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/95/Vue.js_Logo_2.svg/1200px-Vue.js_Logo_2.svg.png',
       flipped: false,
       id: 4,
       pairId: 2,
     },
     {
       name: 'Card 5',
-      image: 'string',
+      image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/cf/Angular_full_color_logo.svg/1200px-Angular_full_color_logo.svg.png',
       flipped: false,
       id: 5,
       pairId: 3,
     },
     {
       name: 'Card 6',
-      image: 'string',
+      image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/cf/Angular_full_color_logo.svg/1200px-Angular_full_color_logo.svg.png',
       flipped: false,
       id: 6,
       pairId: 3
     },
     {
       name: 'Card 7',
-      image: 'string',
+      image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/4c/Typescript_logo_2020.svg/1200px-Typescript_logo_2020.svg.png',
       flipped: false,
       id: 7,
       pairId: 4,
     },
     {
       name: 'Card 8',
-      image: 'string',
+      image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/4c/Typescript_logo_2020.svg/1200px-Typescript_logo_2020.svg.png',
       flipped: false,
       id: 8,
       pairId: 4,
+    },
+    {
+      name: 'Card 9',
+      image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/99/Unofficial_JavaScript_logo_2.svg/1200px-Unofficial_JavaScript_logo_2.svg.png',
+      flipped: false,
+      id: 9,
+      pairId: 5,
+    },
+    {
+      name: 'Card 10',
+      image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/99/Unofficial_JavaScript_logo_2.svg/1200px-Unofficial_JavaScript_logo_2.svg.png',
+      flipped: false,
+      id: 10,
+      pairId: 5,
+    },
+    {
+      name: 'Card 11',
+      image: 'https://pbs.twimg.com/profile_images/1730334391501488129/G0R0sjHH_400x400.jpg',
+      flipped: false,
+      id: 11,
+      pairId: 6,
+    },
+    {
+      name: 'Card 12',
+      image: 'https://pbs.twimg.com/profile_images/1730334391501488129/G0R0sjHH_400x400.jpg',
+      flipped: false,
+      id: 12,
+      pairId: 6,
+    },
+    {
+      name: 'Card 13',
+      image: 'https://miro.medium.com/v2/resize:fit:400/1*OARpkeBkn_Tw3vk8H769OQ.png',
+      flipped: false,
+      id: 13,
+      pairId: 7,
+    },
+    {
+      name: 'Card 14',
+      image: 'https://miro.medium.com/v2/resize:fit:400/1*OARpkeBkn_Tw3vk8H769OQ.png',
+      flipped: false,
+      id: 14,
+      pairId: 7,
+    },
+    {
+      name: 'Card 15',
+      image: 'https://www.drupal.org/files/project-images/nextjs-icon-dark-background.png',
+      flipped: false,
+      id: 15,
+      pairId: 8,
+    },
+    {
+      name: 'Card 16',
+      image: 'https://www.drupal.org/files/project-images/nextjs-icon-dark-background.png',
+      flipped: false,
+      id: 16,
+      pairId: 8,
     },
   ]);
 
@@ -120,7 +176,7 @@ const CardGrid = () => {
     }
   }, [flippedCards]);
   return(
-    <div className="mx-auto grid grid-cols-4 gap-4 w-[70vw]">
+    <div className="mx-auto grid grid-cols-4 w-[40vw]">
       {cards.map( (card) => (
         <Card
           key={card.id}
