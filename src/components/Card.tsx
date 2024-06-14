63ee06d427634e2d66185f0c2eb85fb1e8d3a015
CleverDarkSolutions <RotmgADAS@o2.pl>
2024-06-14 02:59:51 +0200

    Flipping of cards controlled by parent component achieved

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -3,12 +3,14 @@ import React from 'react';
 export type CardProps = {
     name: string,
     image: string,
-    id: number
+    id: number,
+    flip: (id: number) => void,
+    flipped: boolean,
 }
 const Card = (props: CardProps) => {
   return(
     <div className="w-full h-40 m-4 border-2 border-blue-500">
-      {props.name}
+      {props.flipped ? <div className="bg-black text-white h-full">flipped</div> : <div className="h-full" onClick={()=>props.flip(props.id)}>{props.name}</div>}
     </div>
   );
 };
--- src/components/CardGrid.tsx
+++ src/components/CardGrid.tsx
@@ -1,54 +1,76 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Card from './Card';
 
-const cards = [
-  {
-    name: 'Card 1',
-    image: 'string',
-    id: Math.floor(Math.random() * 1000)
-  },
-  {
-    name: 'Card 2',
-    image: 'string',
-    id: Math.floor(Math.random() * 1000)
-  },
-  {
-    name: 'Card 3',
-    image: 'string',
-    id: Math.floor(Math.random() * 1000)
-  },
-  {
-    name: 'Card 4',
-    image: 'string',
-    id: Math.floor(Math.random() * 1000)
-  },
-  {
-    name: 'Card 5',
-    image: 'string',
-    id: Math.floor(Math.random() * 1000)
-  },
-  {
-    name: 'Card 6',
-    image: 'string',
-    id: Math.floor(Math.random() * 1000)
-  },
-  {
-    name: 'Card 7',
-    image: 'string',
-    id: Math.floor(Math.random() * 1000)
-  },
-  {
-    name: 'Card 8',
-    image: 'string',
-    id: Math.floor(Math.random() * 1000)
-  },
-];
+
 
 const CardGrid = () => {
+  const [cards, setCards] = useState([
+    {
+      name: 'Card 1',
+      image: 'string',
+      flipped: false,
+      id: 1
+    },
+    {
+      name: 'Card 2',
+      image: 'string',
+      flipped: false,
+      id: 1
+    },
+    {
+      name: 'Card 3',
+      image: 'string',
+      flipped: false,
+      id: 2,
+    },
+    {
+      name: 'Card 4',
+      image: 'string',
+      flipped: false,
+      id: 2
+    },
+    {
+      name: 'Card 5',
+      image: 'string',
+      flipped: false,
+      id: 3
+    },
+    {
+      name: 'Card 6',
+      image: 'string',
+      flipped: false,
+      id: 3
+    },
+    {
+      name: 'Card 7',
+      image: 'string',
+      flipped: false,
+      id: 4
+    },
+    {
+      name: 'Card 8',
+      image: 'string',
+      flipped: false,
+      id: 4
+    },
+  ]);
+  const flipCard = (id: number) => {
+    const newCards = cards.map(card =>
+      card.id === id ? { ...card, flipped: true } : card
+    );
+    setCards(newCards);
+  };
   return(
     <div className="mx-auto grid grid-cols-4 gap-4 w-[70vw]">
       {cards.map( (card) => (
-        <Card key={card.id} name={card.name} image={card.image} id={card.id}/>
+        <Card
+          key={card.id}
+          name={card.name}
+          image={card.image}
+          id={card.id}
+          flip={() => flipCard(card.id)}
+          flipped={card.flipped}
+        />
       ))}
     </div>
   );
